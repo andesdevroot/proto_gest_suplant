2e9ae8b04968dddc6b7dc55adffebed566739427
andesdevroot <andes.dev.root@gmail.com>
2019-05-10 10:23:40 -0400

    update front

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :socios
+
+root to: redirect('/socios')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
