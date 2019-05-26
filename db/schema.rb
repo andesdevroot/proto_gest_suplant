54917ab825e422ba94dc73b3cc3c660b655c8936
andesdevroot <andes.dev.root@gmail.com>
2019-05-26 10:50:47 -0400

    se cambia base de datos app de sqlite a psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2019_05_10_145247) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "consulta", force: :cascade do |t|
     t.string "tema"
     t.string "diregente"
     t.date "fecha"
     t.text "descripción"
-    t.integer "socio_id"
+    t.bigint "socio_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["socio_id"], name: "index_consulta_on_socio_id"
@@ -42,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_05_10_145247) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "consulta", "socios"
 end
